f49e63547cc6845c2d6c5812ad524b8e480f26d5
shicoy <shico@quatro-digital.com>
2021-10-04 00:23:27 +0300

    updates

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const path = require('path');
+var history = require('connect-history-api-fallback');
 const app = express();
+const staticFileMiddleware = express.static(path.join(__dirname, '/my-app/dist'));
+
 //const port = 8888;
 const port = process.env.port || 8888;
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,6 +29,14 @@ const corsOpts = {
 
 app.use(cors(corsOpts));
 
+app.use(staticFileMiddleware);
+app.use(history({
+    index: '/index.html'
+}));
+
+app.use(staticFileMiddleware);
+
+//app.use(express.static('/my-app/dist'));
 app.use(express.static(path.join(__dirname, '/my-app/dist')));
 
 app.post('/api/validate', (req, res) => {
@@ -176,7 +187,7 @@ app.post('/api/entities', (req, res) => {
     const workspace = req.body.workspace;
     const analytic = req.body.analytic;
     var client;
-    
+
     //an example to list resource groups in a subscription
     msRestAzure
         .loginWithServicePrincipalSecret(clientId, secret, domain)
@@ -221,7 +232,7 @@ app.post('/api/editentitysingle', (req, res) => {
             client = new AzureServiceClient(creds);
             let options = {
                 method: "GET",
-                url: 'https://management.azure.com' + workspace + '/providers/Microsoft.SecurityInsights/incidents/'+incident+'?api-version=2021-04-01',
+                url: 'https://management.azure.com' + workspace + '/providers/Microsoft.SecurityInsights/incidents/' + incident + '?api-version=2021-04-01',
                 headers: {
                     "user-agent": "MyTestApp/1.0",
                 },
@@ -229,17 +240,17 @@ app.post('/api/editentitysingle', (req, res) => {
             return client.sendRequest(options);
         })
         .then((result) => {
-            
+
             //delete result.properties.lastModifiedUtc;
 
-            result.properties.description = result.properties.description + ' \n '+(req.body.entity[5]);
+            result.properties.description = result.properties.description + ' \n ' + (req.body.entity[5]);
             msRestAzure
                 .loginWithServicePrincipalSecret(clientId, secret, domain)
                 .then((creds) => {
                     client = new AzureServiceClient(creds);
                     let options = {
                         method: "PUT",
-                        url: 'https://management.azure.com' + workspace + '/providers/Microsoft.SecurityInsights/incidents/'+incident+'?api-version=2021-04-01',
+                        url: 'https://management.azure.com' + workspace + '/providers/Microsoft.SecurityInsights/incidents/' + incident + '?api-version=2021-04-01',
                         headers: {
                             "user-agent": "MyTestApp/1.0",
                         },
@@ -262,7 +273,6 @@ app.post('/api/editentitysingle', (req, res) => {
         });
 });
 
-
 app.post('/api/editentity', (req, res) => {
     const msrestazure = require('ms-rest-azure');
     const LogAnalyticsClient = require('azure-loganalytics');
@@ -284,20 +294,20 @@ app.post('/api/editentity', (req, res) => {
             const workspaceId = req.body.workspace;
             var body = {
                 //query: 'let AnalyticRuleIds = dynamic("'+req.body.analytic+'"); \n let SecurityAlertTable = SecurityAlert \n | extend Name_ = tostring(parse_json(Entities)[0].Name); \n SecurityIncident \n | where RelatedAnalyticRuleIds[0] in (AnalyticRuleIds) \n | extend SystemAlertId = tostring(AlertIds[0]) \n | join kind=inner SecurityAlertTable on $left.SystemAlertId == $right.SystemAlertId \n | distinct IncidentName, tostring(AlertIds[0]), Name_',
-                query: 'let AnalyticRuleIds = dynamic("'+req.body.analytic+'"); \n let SecurityAlertTable = SecurityAlert \n | extend Name_ = tostring(parse_json(Entities)[0].Name); \n SecurityIncident \n | where RelatedAnalyticRuleIds[0] in (AnalyticRuleIds) \n | extend SystemAlertId = tostring(AlertIds[0]) \n | join kind=inner SecurityAlertTable on $left.SystemAlertId == $right.SystemAlertId \n | extend Link = strcat(\'http://localhost:8080/incident/\',IncidentNumber,\'/\',Name_)  \n | distinct IncidentName, tostring(AlertIds[0]), Name_, IncidentNumber, Title, Link',
+                query: 'let AnalyticRuleIds = dynamic("' + req.body.analytic + '"); \n let SecurityAlertTable = SecurityAlert \n | extend Name_ = tostring(parse_json(Entities)[0].Name); \n SecurityIncident \n | where RelatedAnalyticRuleIds[0] in (AnalyticRuleIds) \n | extend SystemAlertId = tostring(AlertIds[0]) \n | join kind=inner SecurityAlertTable on $left.SystemAlertId == $right.SystemAlertId \n | extend Link = strcat(\'https://sentinelapps.azurewebsites.net/incident/\',IncidentNumber,\'/\',Name_)  \n | distinct IncidentName, tostring(AlertIds[0]), Name_, IncidentNumber, Title, Link',
             };
 
             client.query(workspaceId, body).then((result) => {
                 return result;
             })
-            .then((result) => {
-                res.send(result);
-                return;
-            })
-            .catch((err) => {
-                //console.dir(err, { depth: null, colors: true });
-                res.send(err);
-            });
+                .then((result) => {
+                    res.send(result);
+                    return;
+                })
+                .catch((err) => {
+                    //console.dir(err, { depth: null, colors: true });
+                    res.send(err);
+                });
         }
     );
     return;
@@ -331,13 +341,13 @@ app.post('/api/sendquery', (req, res) => {
             client.query(workspaceId, body).then((result) => {
                 return result;
             })
-            .then((result) => {
-                res.send(result);
-            })
-            .catch((err) => {
-                //console.dir(err, { depth: null, colors: true });
-                res.send(err);
-            });
+                .then((result) => {
+                    res.send(result);
+                })
+                .catch((err) => {
+                    //console.dir(err, { depth: null, colors: true });
+                    res.send(err);
+                });
         }
     );
     return;
@@ -345,10 +355,21 @@ app.post('/api/sendquery', (req, res) => {
 });
 
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/my-app/dist/index.html'));
-  });
-  
+});
+
+app.all("*", (_req, res) => {
+    try {
+        //res.send(path.join(__dirname, '/my-app/dist/index.html'));
+        
+        res.sendFile(path.join(__dirname, '/my-app/dist/index.html'));
+    } catch (error) {
+        res.json({ success: false, message: "Something went wrong" });
+    }
+});
+
+app.use(express.static('/my-app/dist'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
